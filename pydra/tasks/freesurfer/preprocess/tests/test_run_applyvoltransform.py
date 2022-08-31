9c17084e200eff538429bc5fad9fee1b26763f97
Tom Close <tom.g.close@gmail.com>
2022-08-31 14:11:25 +1000

    apply_vol_transform generated using nipype converter

---
+++ pydra/tasks/freesurfer/preprocess/tests/test_run_applyvoltransform.py
@@ -0,0 +1,26 @@
+import os, pytest
+from pathlib import Path
+from ..applyvoltransform import ApplyVolTransform
+
+
+@pytest.mark.xfail(
+    "ANTSPATH" not in os.environ, reason="no Freesurfer found", raises=FileNotFoundError
+)
+@pytest.mark.parametrize("inputs, outputs", [])
+def test_ApplyVolTransform(test_data, inputs, outputs):
+    in_file = Path(test_data) / "test.nii.gz"
+    if inputs is None:
+        inputs = {}
+    for key, val in inputs.items():
+        try:
+            inputs[key] = eval(val)
+        except:
+            pass
+    task = ApplyVolTransform(in_file=in_file, **inputs)
+    assert set(task.generated_output_names) == set(
+        ["return_code", "stdout", "stderr"] + outputs
+    )
+    res = task()
+    print("RESULT: ", res)
+    for out_nm in outputs:
+        assert getattr(res.output, out_nm).exists()
