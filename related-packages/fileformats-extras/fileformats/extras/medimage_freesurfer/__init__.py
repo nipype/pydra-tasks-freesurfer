2770c4ded33175b1ff323a53d441c32e08c4ce08
Tom Close <tom.g.close@gmail.com>
2024-03-20 17:08:51 +1100

    added in fresh generated specs

--- related-packages/fileformats-extras/fileformats/extras/medimage_freesurfer/__init__.py
+++ related-packages/fileformats-extras/fileformats/extras/medimage_freesurfer/__init__.py
@@ -1,108 +1,104 @@
+from ._version import __version__  # noqa: F401
 from pathlib import Path
 import typing as ty
 from random import Random
-from fileformats.core import FileSet
+from fileformats.core import FileSet, SampleFileGenerator
 from fileformats.medimage_freesurfer import (
-    Orig,
-    Xfm,
-    Lta,
-    Stats,
-    Avg_curv,
-    Inflated,
-    Nofix,
-    Pial,
-    M3z,
-    Thickness,
-    Annot,
     Label,
-    Ctab,
-    Out,
-    Area,
-    White,
+    Lta,
+    Orig,
     Reg,
+    Annot,
+    Pial,
+    Nofix,
+    Ctab,
+    Xfm,
+    Area,
+    M3z,
+    Stats,
+    White,
+    Thickness,
+    Inflated,
+    Out,
 )
 
 
+
 @FileSet.generate_sample_data.register
-def gen_sample_orig_data(orig: Orig, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
+def gen_sample_label_data(label: Label, generator: SampleFileGenerator) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_xfm_data(xfm: Xfm, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
+def gen_sample_lta_data(lta: Lta, generator: SampleFileGenerator) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_lta_data(lta: Lta, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
+def gen_sample_orig_data(orig: Orig, generator: SampleFileGenerator) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_stats_data(stats: Stats, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
+def gen_sample_reg_data(reg: Reg, generator: SampleFileGenerator) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_avg_curv_data(avg_curv: Avg_curv, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
+def gen_sample_annot_data(annot: Annot, generator: SampleFileGenerator) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_inflated_data(inflated: Inflated, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
+def gen_sample_pial_data(pial: Pial, generator: SampleFileGenerator) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_nofix_data(nofix: Nofix, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
+def gen_sample_nofix_data(nofix: Nofix, generator: SampleFileGenerator) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_pial_data(pial: Pial, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
+def gen_sample_ctab_data(ctab: Ctab, generator: SampleFileGenerator) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_m3z_data(m3z: M3z, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
+def gen_sample_xfm_data(xfm: Xfm, generator: SampleFileGenerator) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_thickness_data(thickness: Thickness, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
+def gen_sample_area_data(area: Area, generator: SampleFileGenerator) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_annot_data(annot: Annot, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
+def gen_sample_m3z_data(m3z: M3z, generator: SampleFileGenerator) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_label_data(label: Label, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
+def gen_sample_stats_data(stats: Stats, generator: SampleFileGenerator) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_ctab_data(ctab: Ctab, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
+def gen_sample_white_data(white: White, generator: SampleFileGenerator) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_out_data(out: Out, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
+def gen_sample_thickness_data(thickness: Thickness, generator: SampleFileGenerator) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_area_data(area: Area, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
+def gen_sample_inflated_data(inflated: Inflated, generator: SampleFileGenerator) -> ty.Iterable[Path]:
     raise NotImplementedError
 
 
 @FileSet.generate_sample_data.register
-def gen_sample_white_data(white: White, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
-    raise NotImplementedError
-
-
-@FileSet.generate_sample_data.register
-def gen_sample_reg_data(reg: Reg, dest_dir: Path, seed: ty.Union[int, Random] = 0, stem: ty.Optional[str] = None) -> ty.Iterable[Path]:
+def gen_sample_out_data(out: Out, generator: SampleFileGenerator) -> ty.Iterable[Path]:
     raise NotImplementedError
--- related-packages/fileformats/fileformats/medimage_freesurfer/__init__.py
+++ related-packages/fileformats/fileformats/medimage_freesurfer/__init__.py
@@ -1,13 +1,8 @@
+from ._version import __version__  # noqa: F401
 from fileformats.generic import File
 
-
-class Orig(File):
-    ext = ".orig"
-    binary = True
-
-
-class Xfm(File):
-    ext = ".xfm"
+class Label(File):
+    ext = ".label"
     binary = True
 
 
@@ -16,38 +11,13 @@ class Lta(File):
     binary = True
 
 
-class Stats(File):
-    ext = ".stats"
+class Orig(File):
+    ext = ".orig"
     binary = True
 
 
-class Avg_curv(File):
-    ext = ".avg_curv"
-    binary = True
-
-
-class Inflated(File):
-    ext = ".inflated"
-    binary = True
-
-
-class Nofix(File):
-    ext = ".nofix"
-    binary = True
-
-
-class Pial(File):
-    ext = ".pial"
-    binary = True
-
-
-class M3z(File):
-    ext = ".m3z"
-    binary = True
-
-
-class Thickness(File):
-    ext = ".thickness"
+class Reg(File):
+    ext = ".reg"
     binary = True
 
 
@@ -56,8 +26,13 @@ class Annot(File):
     binary = True
 
 
-class Label(File):
-    ext = ".label"
+class Pial(File):
+    ext = ".pial"
+    binary = True
+
+
+class Nofix(File):
+    ext = ".nofix"
     binary = True
 
 
@@ -66,8 +41,8 @@ class Ctab(File):
     binary = True
 
 
-class Out(File):
-    ext = ".out"
+class Xfm(File):
+    ext = ".xfm"
     binary = True
 
 
@@ -76,11 +51,31 @@ class Area(File):
     binary = True
 
 
+class M3z(File):
+    ext = ".m3z"
+    binary = True
+
+
+class Stats(File):
+    ext = ".stats"
+    binary = True
+
+
 class White(File):
     ext = ".white"
     binary = True
 
 
-class Reg(File):
-    ext = ".reg"
+class Thickness(File):
+    ext = ".thickness"
+    binary = True
+
+
+class Inflated(File):
+    ext = ".inflated"
+    binary = True
+
+
+class Out(File):
+    ext = ".out"
     binary = True
